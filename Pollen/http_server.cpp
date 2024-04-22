128ea0b1db70f5d7f855928a3db528ec6ee2463b
Botond Horvath <hello@boti7.com>
2024-04-22 15:56:32 +0200

    Take picture and send it as HTTP response

--- Pollen/http_server.cpp
+++ Pollen/http_server.cpp
@@ -2,8 +2,10 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include "SD_MMC.h"
+#include "esp_camera.h"
 
 AsyncWebServer server(80);
+camera_fb_t * fb = nullptr;
 
 class PollenServer {
   public:
@@ -15,6 +17,36 @@ void PollenServer::init() {
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(SD_MMC, "/hair.jpg", "image/jpeg");
+  });
+
+  server.on("/capture", HTTP_GET, [](AsyncWebServerRequest *request) {
+    Serial.println("request begin");
+
+    fb = esp_camera_fb_get();
+    AsyncWebServerResponse *response = request->beginChunkedResponse("image/jpeg", [](uint8_t *buffer, size_t maxLen, size_t index) -> size_t {
+      //Write up to "maxLen" bytes into "buffer" and return the amount written.
+      //index equals the amount of bytes that have been already sent
+      //You will be asked for more data until 0 is returned
+      //Keep in mind that you can not delay or yield waiting for more data!
+
+      int len = fb->len - index;
+      if (len > maxLen) {
+        len = maxLen;
+      }
+      else if (len <= 0) {
+        Serial.println("esp_camera_fb_return");
+        esp_camera_fb_return(fb);
+        return 0;
+      }
+
+      memcpy(buffer, fb->buf + index, len);
+
+      return len;
+    });
+
+    request->send(response);
+
+    Serial.println("request end");
   });
 
   server.serveStatic("/", SD_MMC, "/");
