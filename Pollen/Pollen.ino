5974e7e03d6a4f9ab75afc8c98e00ea6c5694e82
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2024-04-29 13:35:49 +0200

    cleanup

--- Pollen/Pollen.ino
+++ Pollen/Pollen.ino
@@ -1,30 +1,9 @@
 #include "esp_camera.h"
-
 #include "camera.h"
 #include "wifi_client.h"
 #include "http_server.h"
 #include "time.h"
 #include "time_config.h"
-
-
-
-// void test_camera() {
-//   Serial.println("test_camera");
-//   camera_fb_t * fb = NULL;
-  
-//   fb = esp_camera_fb_get();
-
-//   if(!fb) {
-//     Serial.println("Camera capture failed");
-//     return;
-//   }
-//   //change to send file through wifi
-//   //write_file(filename, fb->buf, fb->len);
-//   esp_camera_fb_return(fb);
-
-//   Serial.println("Camera capture success");
-// }
-
 
 void setup() {
   Serial.begin(115200);
@@ -33,10 +12,7 @@ void setup() {
   init_wifi();
   init_camera();
   init_server();
-
-  //time
   init_time();
-  
   
 }
 
