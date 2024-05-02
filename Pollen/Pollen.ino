3ed5b9f21ece67ede8316c284a77938ca113ad20
MagnusTrojahn <90682418+MagnusTrojahn@users.noreply.github.com>
2024-05-02 11:09:32 +0200

    ESP32 succesfully connects to API

--- Pollen/Pollen.ino
+++ Pollen/Pollen.ino
@@ -1,7 +1,7 @@
 #include "esp_camera.h"
 #include "camera.h"
 #include "wifi_client.h"
-#include "http_server.h"
+//#include "http_server.h"
 #include "http_client.h"
 #include "time.h"
 #include "time_config.h"
@@ -12,8 +12,8 @@ void setup() {
 
   init_wifi();
   init_camera();
-  init_server();
-  init_time();
+  //init_server();
+  //init_time();
   
   delay(5000);
   post_picture();
--- Pollen/http_client.cpp
+++ Pollen/http_client.cpp
@@ -1,48 +1,54 @@
 #include <WiFi.h>
 #include "esp_camera.h"
 
-String serverName = "127.0.0.1";
+String serverName = "192.168.1.142";
 int port = 5194;
 String serverPath = "/Image";
 WiFiClient client;
 
 void post_picture() {
-  if(WiFi.status()== WL_CONNECTED){
+  if (WiFi.status() == WL_CONNECTED) {
     if (client.connect(serverName.c_str(), port)) {
       Serial.println("Connection successful!");
 
-      camera_fb_t * fb = nullptr;
-      fb = esp_camera_fb_get();
-      
-      String head = "--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"imageFile\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+      camera_fb_t *fb = esp_camera_fb_get();
+      if (!fb) {
+        Serial.println("Failed to capture image");
+        return;
+      }
+
+      String head = "--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"file\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
       String tail = "\r\n--RandomNerdTutorials--\r\n";
+      size_t imageLen = fb->len;
+      size_t extraLen = head.length() + tail.length();
+      size_t totalLen = imageLen + extraLen;
 
-      uint16_t imageLen = fb->len;
-      uint16_t extraLen = head.length() + tail.length();
-      uint16_t totalLen = imageLen + extraLen;
-    
+      // Send HTTP POST request headers
       client.println("POST " + serverPath + " HTTP/1.1");
-      client.println("Host: " + serverName);
-      client.println("Content-Length: " + String(totalLen));
+      client.println("Host: " + serverName + ":" + String(port));
       client.println("Content-Type: multipart/form-data; boundary=RandomNerdTutorials");
+      client.println("Content-Length: " + String(totalLen));
       client.println();
+      
+      // Send image data
+      Serial.println("Uploading image");
       client.print(head);
-
-      uint8_t *fbBuf = fb->buf;
-      size_t fbLen = fb->len;
-      for (size_t n=0; n<fbLen; n=n+1024) {
-        if (n+1024 < fbLen) {
-          client.write(fbBuf, 1024);
-          fbBuf += 1024;
-        }
-        else if (fbLen%1024>0) {
-          size_t remainder = fbLen%1024;
-          client.write(fbBuf, remainder);
-        }
-      }   
+      client.write(fb->buf, fb->len);
       client.print(tail);
+      Serial.println("Uploading image done");
+
+      // Wait for response
+      delay(10); // Allow time for server to process
+      while (client.available()) {
+        char c = client.read();
+        Serial.print(c);
+      }
+
+      // Clean up
       client.stop();
       esp_camera_fb_return(fb);
+    } else {
+      Serial.println("Connection failed!");
     }
   }
 }
